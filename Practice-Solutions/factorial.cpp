65bc56eea8d80a39eb8d73e2f4d5d366093edcbb
sudosf <sudosprogrammer@gmail.com>
2022-01-22 22:28:37 +0200

    added factorial problem solution

--- Practice-Solutions/factorial.cpp
+++ Practice-Solutions/factorial.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
+int factorial (int n) {
+    if (n == 0 || n == 1) return 1;
+
+    return n * factorial(n-1);
+}
+
 int main () {
+
+	// optimize for faster reading of input
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << factorial(n) << "\n";
 
     return 0;
 }
