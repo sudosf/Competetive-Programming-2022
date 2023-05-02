d39fd2e6c308563ced8c3343813f793b6f849f24
sudosf <sudosprogrammer@gmail.com>
2023-05-02 23:46:51 +0200

    added bdd template

---
+++ Coding Puzzles/BBD.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+/*   ______Update Class Name!______			*/
+public class BBD {
+
+	public static void main(String[] args) {
+		/*
+		FastReader input = new FastReader();
+	 
+		int T = input.nextInt();
+		for (int i = 0; i < T; i++) {
+			// code here
+		}
+		*/
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(
+				new InputStreamReader(System.in));
+		} // constructor
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				}
+				catch (IOException e) { e.printStackTrace(); }
+			}
+
+			return st.nextToken();
+		} // read single character or string
+
+		int nextInt() { 
+			return Integer.parseInt(next()); 
+		} 
+
+		long nextLong() { 
+			return Long.parseLong(next()); 
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				if (st.hasMoreTokens()) str = st.nextToken("\n");
+				else str = br.readLine();
+			}
+			catch (IOException e) { e.printStackTrace(); }
+			
+			return str;
+		} // read entire line
+
+	} // FastReader class 
+}
